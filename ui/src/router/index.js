db105e35ce8b6add1e6f377a65c26368524fda19
ivan.laid <ivanrobert1992@gmail.com>
2020-02-05 14:44:27 -0800

    updated

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 Vue.use(Router);
 
-const defaultPage = () => import("@/views/templates/DefaultPage.vue");
+const defaultContainer = () => import("@/views/layouts/DefaultContainer.vue");
 
 export default new Router({
   mode: "history",
@@ -11,7 +11,7 @@ export default new Router({
       path: "/",
       redirect: "/",
       name: "Default Page",
-      component: defaultPage,
+      component: defaultContainer,
 
       children: [
         {
@@ -19,73 +19,11 @@ export default new Router({
           name: "Home",
           component: () => import("@/views/pages/Index.vue"),
           meta: {
-            title: `Home - Your Boutique`,
-            noAuth: true
+            title: `Home`,
           }
         },
-        {
-          path: "/shop",
-          name: "Shop",
-          component: () => import("@/views/pages/Shop.vue"),
-          meta: {
-            title: `Shop - Your Boutique`,
-            noAuth: true
-          }
-        },
-        {
-          path: "/my-account",
-          name: "My Account",
-          component: () => import("@/views/pages/MyAccount.vue"),
-          meta: {
-            title: `My Account - Your Boutique`
-          }
-        },
-
-        {
-          path: "/cart",
-          name: "Cart",
-          component: () => import("@/views/pages/CartPage.vue"),
-          meta: {
-            title: `Cart - Your Boutique`
-          }
-        },
-
-        {
-          path: "/transactions",
-          name: "Transactions",
-          component: () => import("@/views/pages/transactions/Index.vue"),
-          meta: {
-            title: `Transactions - Your Boutique`
-          }
-        },
-
-        {
-          path: "/contact",
-          name: "Contact",
-          component: () => import("@/views/pages/Contact.vue"),
-          meta: {
-            title: `Contact - Your Boutique`
-          }
-        }
       ]
     },
-
-    {
-      path: "/shop",
-      component: defaultPage,
-      children: [
-        {
-          path: "single-product/:slug",
-          name: "Singe Product",
-          component: () => import("@/views/pages/SingleProductPage.vue"),
-          meta: {
-            title: `Shop - Singe Product - Your Boutique`,
-            noAuth: true
-          }
-        }
-      ]
-    },
-
     {
       path: "/",
       component: {
@@ -94,15 +32,6 @@ export default new Router({
         }
       },
       children: [
-        {
-          path: "/auth",
-          name: "Auth",
-          component: () => import("@/views/pages/Auth.vue"),
-          meta: {
-            noAuth: true,
-            title: `Auth - Mom's Family Clinic`
-          }
-        },
         {
           path: "*",
           redirect: "/home"
