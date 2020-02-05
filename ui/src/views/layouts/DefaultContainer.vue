687e1ea4681ef16bf23eda6fb44e5c0293da255e
ivan.laid <ivanrobert1992@gmail.com>
2020-02-05 14:49:38 -0800

    updated

---
+++ ui/src/views/layouts/DefaultContainer.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<DefaultHeader/>
+		<router-view></router-view>
+		<DefaultFooter/>
+	</div>
+</template>
+
+<script>
+import DefaultFooter from '@/views/partials/Footer';
+import DefaultHeader from '@/views/partials/Header';
+
+export default {
+	componets: {
+		DefaultHeader,
+		DefaultFooter
+	}
+};
+</script>
